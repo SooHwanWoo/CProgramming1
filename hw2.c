ea86d678d535f4e4098d1ed390cf37e81156919b
SooHwanWoo <swanwoo1@naver.com>
2024-09-19 15:01:15 +0900

    Add C File [hw2.c]

---
+++ hw2.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(void)
+{
+	double num1, num2;
+	printf("Please enter kilometers: ");
+	scanf_s("%lf", &num1);
+	num2 = num1 / 1.609;
+	printf("%.1f km is equal to %.1f miles.", num1, num2);
+}
